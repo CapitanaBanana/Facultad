dca0a688f9e18e1695ad2be4f3cb8e65f9e91d13
María Josefina Martinez <josemartinezosti@gmail.com>
2024-11-11 19:53:55 -0300

    aaa

---
+++ Sexto_Cuatrimestre/Concurrente/Prácticas/Resoluciones/RepasoParcial2.c
@@ -0,0 +1,21 @@
+/* 1. Resolver con PMA el siguiente problema. Se debe modelar el funcionamiento de una casa de venta de repuestos 
+automotores, en la que trabajan V vendedores y que debe atender a C clientes. El modelado debe considerar que: 
+(a) cada cliente realiza un pedido y luego espera a que se lo entreguen; y (b) los pedidos que hacen los clientes son 
+tomados por cualquiera de los vendedores. Cuando no hay pedidos para atender, los vendedores aprovechan para 
+controlar el stock de los repuestos (tardan entre 2 y 4 minutos para hacer esto). Nota: maximizar la concurrencia.
+ */
+
+
+
+
+2. Resolver con ADA el siguiente problema. Se quiere modelar el funcionamiento de un banco, al cual llegan 
+clientes que deben realizar un pago y llevarse su comprobante. Los clientes se dividen entre los regulares y los 
+premium, habiendo R clientes regulares y P clientes premium. Existe un único empleado en el banco, el cual 
+atiende de acuerdo al orden de llegada, pero dando prioridad a los premium sobre los regulares. Si a los 30 
+minutos de llegar un cliente regular no fue atendido, entonces se retira sin realizar el pago. Los clientes premium 
+siempre esperan hasta ser atendidos.
+
+1. En una oficina existen 100 empleados que envían documentos para imprimir en 5 impresoras compartidas. Los 
+pedidos de impresión son procesados por orden de llegada y se asignan a la primera impresora que se encuentre 
+libre. Implemente un programa que permita resolver el problema anterior usando PASAJE DE MENSAJES 
+ASINCRÓNICO (PMA). 
