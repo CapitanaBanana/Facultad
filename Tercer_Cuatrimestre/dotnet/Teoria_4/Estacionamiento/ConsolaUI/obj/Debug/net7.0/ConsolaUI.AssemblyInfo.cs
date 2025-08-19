4156739db47ae0f137bcc5c7eafa8600f62e7484
María Josefina Martinez <josemartinezosti@gmail.com>
2025-08-19 15:54:06 -0300

    Cambio la organización de materias dentro del repo

--- Tercer_Cuatrimestre/dotnet/Teoria_4/Estacionamiento/ConsolaUI/obj/Debug/net7.0/ConsolaUI.AssemblyInfo.cs
+++ Tercer_Cuatrimestre/dotnet/Teoria_4/Estacionamiento/ConsolaUI/obj/Debug/net7.0/ConsolaUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsolaUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+308b5fdc9fc2d49f0ffd6fd4826bae245924c4bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b81aa3eb93980f49d140f9643d1c84e758e1d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsolaUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsolaUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
